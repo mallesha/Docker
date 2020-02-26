ac0271eb3c8f073c23e22bc55825e26dc8384b10
mallesha <mallesha666@gmail.com>
2020-02-26 14:42:53 +0100

    update2 travis file

--- src/App.test.js
+++ src/App.test.js
@@ -7,8 +7,4 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
 
